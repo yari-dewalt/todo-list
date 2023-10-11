52abb34991e90a8de085cceae163dd993e5f1e42
yari-dewalt <yari.dewalt@gmail.com>
2023-10-10 19:29:50 -0700

    Change functionality of the add button to create a form

--- src/index.js
+++ src/index.js
@@ -1,21 +1,49 @@
 import { createItem } from "./todos.js"
+import { updateTodoList } from "./todos.js";
 
 let todo_list = [];
 
-let todo1 = createItem("test_title", "test description blah", "10:00PM", "high");
-
-todo_list.push(todo1);
-console.log(todo1);
-
 const addButton = document.getElementById("add-todo");
+const content = document.getElementById("content");
 
 addButton.addEventListener("click", () => {
-    let title = prompt("give title fam");
-    let description = prompt("give description fam");
-    let dueDate = prompt("give duedate fam");
-    let priority = prompt("give priority fam");
+    // let title = prompt("give title fam");
+    // let description = prompt("give description fam");
+    // let dueDate = prompt("give duedate fam");
+    // let priority = prompt("give priority fam");
 
-    let new_todo = createItem(title, description, dueDate, priority);
-    todo_list.push(new_todo);
-    console.log(todo_list);
+    createForm();
+    content.removeChild(addButton);
+
+    // let new_todo = createItem(title, description, dueDate, priority);
+    // todo_list.push(new_todo);
+
+    // updateTodoList(content, todo_list);
 })
+
+function createForm() {
+    let form = document.createElement("form");
+    let title_input = document.createElement("input");
+    let description_input = document.createElement("input");
+    let dueDate_input = document.createElement("input");
+    let submit_button = document.createElement("button");
+    submit_button.setAttribute("type", "submit");
+    let cancel_button = document.createElement("button");
+    cancel_button.setAttribute("type", "button");
+
+    form.appendChild(title_input);
+    form.appendChild(description_input);
+    form.appendChild(dueDate_input);
+    form.appendChild(submit_button);
+    form.appendChild(cancel_button);
+
+    content.appendChild(form);
+
+    form.addEventListener("submit", (event) => {
+        let new_todo = createItem(title_input.value, description_input.value, dueDate_input.value, "high");
+        todo_list.push(new_todo);
+        updateTodoList(content, todo_list, addButton);
+        console.log(todo_list);
+        event.preventDefault();
+    })
+}
