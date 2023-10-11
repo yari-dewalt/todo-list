40fe2e342d36d2aee496207f7a85fa4fe6e09448
yari-dewalt <yari.dewalt@gmail.com>
2023-10-10 19:29:02 -0700

    Add function to refresh/update the todo list

--- src/todos.js
+++ src/todos.js
@@ -9,4 +9,29 @@ function createItem(title, description, dueDate, priority) {
     return todo;
 }
 
+function updateTodoList(container, todo_list, addButton) {
+    while (content.firstChild) {
+        content.removeChild(content.lastChild);
+    }
+
+    todo_list.forEach((todo) => {
+        let div = document.createElement("div");
+        div.setAttribute("class", "todo");
+
+        let title = document.createElement("h3");
+        title.textContent = todo.title;
+
+        let description = document.createElement("p");
+        description.textContent = todo.description;
+
+        div.appendChild(title);
+        div.appendChild(description);
+    
+        content.appendChild(div);
+    })
+
+    content.appendChild(addButton);
+}
+
 export { createItem };
+export { updateTodoList };
