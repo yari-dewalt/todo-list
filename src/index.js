1740fc2f6cd007645c548b7ce045df36170f0920
yari-dewalt <yari.dewalt@gmail.com>
2023-10-12 14:45:07 -0700

    Improve and further flesh out logic for todo item creation and deletion

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { createItem } from "./todos.js"
 import { updateTodoList } from "./todos.js";
+// import { createForm } from "./form.js";
 
 let todo_list = [];
 
@@ -7,43 +8,48 @@ const addButton = document.getElementById("add-todo");
 const content = document.getElementById("content");
 
 addButton.addEventListener("click", () => {
-    // let title = prompt("give title fam");
-    // let description = prompt("give description fam");
-    // let dueDate = prompt("give duedate fam");
-    // let priority = prompt("give priority fam");
-
     createForm();
     content.removeChild(addButton);
-
-    // let new_todo = createItem(title, description, dueDate, priority);
-    // todo_list.push(new_todo);
-
-    // updateTodoList(content, todo_list);
 })
 
 function createForm() {
     let form = document.createElement("form");
+
     let title_input = document.createElement("input");
+    title_input.className = "title";
+    title_input.placeholder = "Title";
+    title_input.required = true;
+
     let description_input = document.createElement("input");
-    let dueDate_input = document.createElement("input");
+    description_input.className = "description";
+
     let submit_button = document.createElement("button");
-    submit_button.setAttribute("type", "submit");
+    submit_button.type = "submit";
+    submit_button.className = "submit";
+    submit_button.innerHTML = "Add";
+
     let cancel_button = document.createElement("button");
-    cancel_button.setAttribute("type", "button");
+    cancel_button.type = "button";
+    cancel_button.className = "cancel";
+    cancel_button.innerHTML = "Cancel";
 
     form.appendChild(title_input);
     form.appendChild(description_input);
-    form.appendChild(dueDate_input);
     form.appendChild(submit_button);
     form.appendChild(cancel_button);
 
     content.appendChild(form);
 
     form.addEventListener("submit", (event) => {
-        let new_todo = createItem(title_input.value, description_input.value, dueDate_input.value, "high");
+        let new_todo = createItem(title_input.value, description_input.value, "duedate", "high");
         todo_list.push(new_todo);
-        updateTodoList(content, todo_list, addButton);
+        updateTodoList(todo_list, addButton);
         console.log(todo_list);
         event.preventDefault();
+    })
+
+    cancel_button.addEventListener("click", () => {
+        content.removeChild(form);
+        content.appendChild(addButton);
     })
 }
--- src/todos.js
+++ src/todos.js
@@ -9,7 +9,7 @@ function createItem(title, description, dueDate, priority) {
     return todo;
 }
 
-function updateTodoList(container, todo_list, addButton) {
+function updateTodoList(todo_list, addButton) {
     while (content.firstChild) {
         content.removeChild(content.lastChild);
     }
@@ -20,17 +20,38 @@ function updateTodoList(container, todo_list, addButton) {
 
         let title = document.createElement("h3");
         title.textContent = todo.title;
+        title.className = "todo-title";
 
         let description = document.createElement("p");
         description.textContent = todo.description;
+        description.className = "todo-description";
+
+        let due_date = document.createElement("input");
+        due_date.setAttribute("type", "datetime-local");
+        due_date.className = "todo-duedate";
+
+        let remove_button = document.createElement("button");
+        remove_button.innerHTML = "X";
+        remove_button.className = "remove_button";
+
+        remove_button.addEventListener("click", () => {
+            removeTodo(todo_list, todo, addButton);
+            updateTodoList(todo_list, addButton);
+        })
 
         div.appendChild(title);
         div.appendChild(description);
+        div.appendChild(due_date);
+        div.appendChild(remove_button);
     
         content.appendChild(div);
     })
 
     content.appendChild(addButton);
+}
+
+function removeTodo(todo_list, todo) {
+    todo_list.splice(todo_list.indexOf(todo), 1);
 }
 
 export { createItem };
